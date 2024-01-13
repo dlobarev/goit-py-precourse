ee0f343ff123a90a611a00765dba56824dfd204f
dlobarev <dlobarev@gmail.com>
2024-01-14 01:24:35 +0200

    c1

--- hw.py
+++ hw.py
@@ -1,2 +1,6 @@
-print("Hello Dimon")
-print("Hello Git")
+pool = 1000
+quantity = int(input("Enter the number of mailings: "))
+try:
+    chunk = pool // quantity
+except ZeroDivisionError:
+    print('Divide by zero completed!')
